c9c9ba870401d60b50108c7869a9d4f0271d8f2b
Ilan Kleiman <ilankleiman@gmail.com>
2015-05-15 18:45:32 -0400

    update

--- Beta/login/cookie_login.pl
+++ Beta/login/cookie_login.pl
@@ -2,52 +2,57 @@
 
 use CGI::Carp qw( fatalsToBrowser );
 use CGI;
-use MIME::Base64;
 use DBI;
 use CGI::Cookie;
 
 BEGIN
 {
+    $DB_SQL_username = "YellowFeather";
+    $DB_SQL_password = "LegoApril181998!";
+
     $q = new CGI;
     $method = $q->param('method');
-
-    #check if cookie exists. if yes, then try login.
-    # else location = login.php
 
     %cookies = CGI::Cookie->fetch;
 
     if(!defined $cookies{'u_cookie'})
     {
-        print $cookies{'u_cookie'}->value;
-        print $q->header(-Location=>'login/login.php');
-
+        print $q->header(-Location=>'login/login.pl');
+    }
+    if(!defined $cookies{'p_cookie'})
+    {
+        print $q->header(-Location=>'login/login.pl');
     }
     else
     {
         $got_u_cookie = $cookies{'u_cookie'}->value;
         $got_p_cookie = $cookies{'p_cookie'}->value;
 
-        my $dbh_random123164 = DBI->connect("DBI:mysql:database=ilankleiman;host=localhost", "YellowFeather", "!",
+        my $dbh_random123164 = DBI->connect("DBI:mysql:database=ilankleiman;host=localhost", "$DB_SQL_username", "$DB_SQL_password",
         {'RaiseError' => 1});
 
-        my $sth_random123164 = $dbh_random123164->prepare("SELECT u_cookie, p_cookie, username, email, valid FROM users WHERE u_cookie = ? AND p_cookie = ?");
+        my $sth_random123164 = $dbh_random123164->prepare("SELECT u_cookie, p_cookie, username FROM users WHERE u_cookie = ? AND p_cookie = ?");
         $sth_random123164->execute($got_u_cookie, $got_p_cookie) or die "Couldn't execute statement: $DBI::errstr; stopped";
         
         while(my($u_cookie, $p_cookie, $username, $email, $user_valid) = $sth_random123164->fetchrow_array())
         {
-            print $real_username = decode_base64($username);
-            print $real_email = $email;
-            print $real_access = $user_valid;
-            $valid = "ok";
+            $real_username = $username;
+            $real_email = $email;
+            $real_access = $user_valid;
+            $valida = "ok";
         }
         $dbh_random123164->disconnect();
 
-        if(!defined $valid)
+        if($valida !~ "ok")
         {
-            print $cookies{'u_cookie'}->value;
-            print $q->header(-Location=>'login/login.php');
+            print $q->header(-Location=>'login/login.pl');
+        }
+        if(!defined $valida)
+        {
+            print $q->header(-Location=>'login/login.pl');
         }
     }
+    print "Content-Type: text/html\n\n";
     open(STDERR, ">&STDOUT");
 }
 1;
